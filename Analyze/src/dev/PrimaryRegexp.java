ece2f412e1f0279e08187a6cf972f25a35f148a7
Seraphima Zykova <zykovas91@gmail.com>
2014-10-04 19:24:26 +0400

    PrimaryRegexp
    первичный анализ строки - проверка, какой тип (NAME, GENERIC, SIGNATURE, OUTPUT) в строке стоит первым

---
+++ Analyze/src/dev/PrimaryRegexp.java
@@ -0,0 +1,39 @@
+package dev;
+
+public class PrimaryRegexp {
+	
+	private static String nameRegex = "[ \t]*[a-z]+.*";
+	private static String genericRegex = "[ \t]*<.*>.*"; 
+	private static String signatureRegex = "[ \t]*[(].*[)].*";
+	private static String outputRegex = "[ \t]*-[ \t]*>[ \t]*[a-z]+.*";
+	
+	public enum type {NAME, GENERIC, SIGNATURE, OUTPUT, PARSE_ERROR}
+	
+	public PrimaryRegexp() {
+	}
+	
+	public static PrimaryRegexp.type type(String request) {
+		
+		if (request.matches("[ \t]*[a-z]+:.*")) {
+			return type.PARSE_ERROR;
+		}
+		
+		if (request.matches(nameRegex)) {
+			return type.NAME;
+		}
+		
+		if (request.matches(genericRegex)) {
+			return type.GENERIC;
+		}
+		
+		if (request.matches(signatureRegex)) {
+			return type.SIGNATURE;
+		}
+		
+		if (request.matches(outputRegex)) {
+			return type.OUTPUT;
+		}
+		
+		return type.PARSE_ERROR;
+	}
+}
---
+++ Analyze/src/unitTest/PrimaryRegexpTest.java
@@ -0,0 +1,75 @@
+package unitTest;
+
+import org.junit.Test;
+
+import dev.PrimaryRegexp;
+
+import static org.junit.Assert.*;
+
+public class PrimaryRegexpTest {
+
+	@Test
+	public void nameTest() {
+		PrimaryRegexp mreg = new PrimaryRegexp();
+		PrimaryRegexp.type res = PrimaryRegexp.type("map");
+		assertEquals(PrimaryRegexp.type.NAME, res);
+
+		res = PrimaryRegexp.type(" 	map");
+		assertEquals(PrimaryRegexp.type.NAME, res);
+		
+		res = PrimaryRegexp.type("map <> ()");
+		assertEquals(PrimaryRegexp.type.NAME, res);
+		
+		res = PrimaryRegexp.type("ma:p a");
+		assertEquals(PrimaryRegexp.type.PARSE_ERROR, res); 
+		/*
+		 * двоеточие может быть только в генерике и сигнатуре, 
+		 * возможно там просто забыли открыть скобку.
+		 * хотя вообще это лишнее тут,
+		 * пускай этот нейм распознается, а следующий шаблон гарантированно вернет parse_error
+		 * потому что нет шаблона, начинающегося с двоеточия
+		 */
+		
+		res = PrimaryRegexp.type("?lalala");
+		assertEquals(PrimaryRegexp.type.PARSE_ERROR, res);
+	}
+	
+	@Test
+	public void genericTest() {
+		PrimaryRegexp mreg = new PrimaryRegexp();
+		PrimaryRegexp.type res = PrimaryRegexp.type("<T: Trait>");
+		assertEquals(PrimaryRegexp.type.GENERIC, res);
+		
+		res = PrimaryRegexp.type("<>");
+		assertEquals(PrimaryRegexp.type.GENERIC, res);
+		
+		res = PrimaryRegexp.type("<notgeneric: Trait1 + Trait2");
+		assertEquals(PrimaryRegexp.type.PARSE_ERROR, res);
+	}
+	
+	@Test
+	public void signatureTest() {
+		PrimaryRegexp mreg = new PrimaryRegexp();
+		PrimaryRegexp.type res = PrimaryRegexp.type("(a: type1)");
+		assertEquals(PrimaryRegexp.type.SIGNATURE, res);
+		
+		res = PrimaryRegexp.type("()");
+		assertEquals(PrimaryRegexp.type.SIGNATURE, res);
+		
+		res = PrimaryRegexp.type("(a: type1, b: type2");
+		assertEquals(PrimaryRegexp.type.PARSE_ERROR, res);
+	}
+	
+	@Test
+	public void outputTest() {
+		PrimaryRegexp mreg = new PrimaryRegexp();
+		PrimaryRegexp.type res = PrimaryRegexp.type("->type");
+		assertEquals(PrimaryRegexp.type.OUTPUT, res);
+		
+		res = PrimaryRegexp.type(" - > type	");
+		assertEquals(PrimaryRegexp.type.OUTPUT, res);
+		
+		res = PrimaryRegexp.type(" -> Type	");
+		assertEquals(PrimaryRegexp.type.PARSE_ERROR, res);
+	}
+}
