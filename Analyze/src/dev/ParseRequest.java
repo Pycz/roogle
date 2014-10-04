9f08a1348994f1cb954de1883ce47d0dce849386
Seraphima Zykova <zykovas91@gmail.com>
2014-10-04 19:23:11 +0400

    parse string from JSON

---
+++ Analyze/src/dev/ParseRequest.java
@@ -0,0 +1,38 @@
+package dev;
+
+import java.io.UnsupportedEncodingException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ParseRequest {
+
+	public static String convertUTF8toUnicode(byte[] utfString) {
+		
+		try {
+			String str = new String(utfString, "US-ASCII");
+			return str;
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;	
+	}
+	
+	public static JSONObject parseJSON (String json) {
+		
+		JSONParser parser = new JSONParser();
+		Object obj;
+		try {
+			obj = parser.parse(json);
+			JSONObject jsonObj = (JSONObject) obj;
+			return jsonObj;
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}	
+}
