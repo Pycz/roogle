5ada851d11f6a376926ebd2a3bbc2ef7b2f48806
alexei <stephanov.al@gmail.com>
2014-10-11 22:32:00 +0400

    create class AnalyzeGeneric

---
+++ Analyze/src/dev/AnalyzeGeneric.java
@@ -0,0 +1,45 @@
+package dev;
+
+import java.util.LinkedList;
+
+
+public class AnalyzeGeneric {
+	/**
+	 * 
+	 * @param generic
+	 * @return LinkedList [type [GenericsList]]
+	 */
+	public static LinkedList<LinkedList> parseGeneric(String generic){ 
+		LinkedList<LinkedList> genericList = new LinkedList();
+		
+		generic = generic.substring(1, generic.length()-1);
+		String[] genericSplitByComa = generic.split(",");
+		for (String s: genericSplitByComa){
+			genericList.add(AnalyzeOneGenericRequest(s));
+		}
+		
+		return genericList; 
+	}
+	
+	private static LinkedList AnalyzeOneGenericRequest(String oneGeneric){
+		LinkedList typeAndGenerics = new LinkedList();
+		
+		String[] oneGenericSplitByColon = oneGeneric.split(":");
+		String type = oneGenericSplitByColon[0];
+		typeAndGenerics.add(type);
+		typeAndGenerics.add(AnalyzeGenericsInOneGenericRequest(oneGenericSplitByColon[1]));
+		return typeAndGenerics;
+	}
+	
+	private static LinkedList AnalyzeGenericsInOneGenericRequest(String generics){
+		LinkedList genericsList = new LinkedList();
+		
+		String[] genericsByPlus = generics.split("\\+");
+		for (String generic : genericsByPlus){
+			genericsList.add(generic);
+		}
+		return genericsList;
+	}
+	
+		
+}
