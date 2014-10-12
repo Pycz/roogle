a38587280e2de2eeb54448f58129477c6432891c
alexei <stephanov.al@gmail.com>
2014-10-12 13:33:22 +0400

    create AnalyzeSignature class and unit test for it

---
+++ Analyze/src/dev/AnalyzeSignature.java
@@ -0,0 +1,49 @@
+package dev;
+
+import java.util.LinkedList;
+import java.util.regex.Pattern;
+
+public class AnalyzeSignature {
+	
+	public static LinkedList parseSignature(String signature){
+		LinkedList signatureList = new LinkedList();
+		
+		signature = signature.substring(1, signature.length()-1);
+		if (!signature.equals("")){
+			String[] signatureSplitByComa = signature.split(",");
+			for (String el: signatureSplitByComa){
+				try{
+					signatureList.add(AnalyzeOneSignatureRequest(el));
+				}
+				catch(Exception e){
+					return null;
+				}
+			}
+		}else {return null;}
+		return signatureList;
+	}
+	
+	private static LinkedList AnalyzeOneSignatureRequest(String oneSignature) throws Exception{
+		LinkedList varAndType = new LinkedList();
+		
+		String[] oneSignatureSplitByColon = oneSignature.split(":");
+		String var = oneSignatureSplitByColon[0];
+		var = var.replaceAll(" ", "");
+		String type = oneSignatureSplitByColon[1];
+		type = type.replaceAll(" ", "");
+		Pattern patternForVar = Pattern.compile("[A-Z]");
+		String varFirstSing = var.substring(0, 1);
+		java.util.regex.Matcher isLetter = patternForVar.matcher(varFirstSing.toUpperCase());
+		if (!(varFirstSing.toLowerCase()).equals(varFirstSing) || !isLetter.matches()){
+			throw new Exception();
+		}
+		varAndType.add(var);
+		String typeFirstSing = type.substring(0, 1);
+		isLetter = patternForVar.matcher(typeFirstSing.toUpperCase());
+		if (!(typeFirstSing.toUpperCase()).equals(typeFirstSing) || !isLetter.matches()){
+			throw new Exception();
+		}
+		varAndType.add(type);
+		return varAndType;
+	}
+}
