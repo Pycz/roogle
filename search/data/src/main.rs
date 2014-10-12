9f7f3e80959b245c19613a9b49be3ff021fe0fe3
Marsel Zaripov <marszaripov@gmail.com>
2014-10-12 18:34:19 +0400

    add command line arguments

--- search/data/src/main.rs
+++ search/data/src/main.rs
@@ -3,19 +3,47 @@ extern crate rustdoc;
 extern crate rustc;
 extern crate syntax;
 extern crate serialize;
+extern crate getopts;
 
+use std::os;
 use rustdoc::core;
 use rustdoc::clean;
 use std::collections::HashMap;
 use serialize::json::{Json, ToJson};
 use rustdoc::fold::DocFolder;
+use getopts::{optflag,getopts,OptGroup};
 mod cache;
 mod json;
 
 
+fn print_usage(program: &str, opts: &[OptGroup]) {
+    println!("Usage: {} <source>", program);
+    for opt in opts.iter() {
+        println!("-{} --{}\t{}", opt.short_name, opt.long_name, opt.desc);
+    }
+
+}
+
+
 fn main() {
-    let cratefile = "/home/marsel/programming/rtest/src/lib.rs";
-    let cr = Path::new(cratefile);
+    let args: Vec<String> = os::args();
+    let program = args[0].clone();
+    let opts = [
+        optflag("h", "help", "Print this help menu"),
+    ];
+
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => m,
+        Err(f) => fail!(f.to_string())
+    };
+
+    if matches.opt_present("h") || matches.free.len() != 1 {
+        print_usage(program.as_slice(), opts);
+        return;
+    }
+
+    let cratefile = &matches.free[0];
+    let cr = Path::new(cratefile.as_slice());
     let libs: Vec<Path> = vec![Path::new("/usr/local/lib/rustlib/i686-unknown-linux-gnu/lib")];
     let cfgs: Vec<String> = vec![];
     let externs: core::Externs = HashMap::new();
